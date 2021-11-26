145d6c0a4ad0dbb73d3f415c93f4939f12aa21c5
Crowdbotics <team@crowdbotics.com>
2021-11-26 16:38:49 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+export const globalOptions = {
+  name: "basketballonaplane_32161",
+  url: "https://basketballonaplane_32161.botics.co",
+  api: "https://basketballonaplane_32161.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    "copy": "Routes available!"
+  }
+}
